7aba090688f011d58e23f1c0321584ac400d77b9
Jiya2301 <jiyasingla26@gmail.com>
2023-09-27 13:24:04 -0700

    Finished up to and including turn test: can run turn test once to make sure, but need to work on localization test now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunnertuningfiles/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunnertuningfiles/DriveConstants.java
@@ -47,7 +47,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.88976; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 16; // in
+    public static double TRACK_WIDTH = 16.55; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -68,7 +68,7 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 49;
     public static double MAX_ACCEL = 30;
-    public static double MAX_ANG_VEL = Math.toRadians(60);
+    public static double MAX_ANG_VEL = .6;
     public static double MAX_ANG_ACCEL = Math.toRadians(60);
 
     /*
